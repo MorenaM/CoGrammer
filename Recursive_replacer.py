3fd4d03101ef2b6ad3733a93c9b8d85fe988544d
MorenaM <42901294+MorenaM@users.noreply.github.com>
2021-01-12 19:24:40 +0200

    Update Recursive_replacer.py
    Implemented the replace recursive function and added comments

--- Recursive_replacer.py
+++ Recursive_replacer.py
@@ -1,4 +1,21 @@
 
+def replace(string, subString, replacer):	 
+	
+    # Find the index of the substring
+    index = string.find(subString)
+    
+    # Base case if string does not have the substring
+	# return the string
+    if index == -1:
+        return string
+         
+    # If not then we have to call 
+    # recursion from after the index + length of substring
+    # then add recursion result to the elements before substring
+    # and add the replacer and return it
+    smallOutput = replace(string[index+len(subString):], subString, replacer)
+    return string[:index] + replacer + smallOutput
+       
 # Driver code
 if __name__ == "__main__":
 
@@ -8,4 +25,4 @@ if __name__ == "__main__":
 
     # Function call
     newString = replace(string, subString, replacer)
-    print("Your new string is: " + newString)
+    print("\nYour new string is: " + newString)
